922b4fa844f517cf668a7c3aa89050bc34290170
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2018-10-30 12:46:25 +0100

    Simplify symlink script

--- scripts/symlink_trace_orig.py
+++ scripts/symlink_trace_orig.py
@@ -29,38 +29,17 @@ if not os.path.exists(trace_orig):
 if os.path.islink("trace_orig"):
     # The symlink 'trace_orig' must have been created by this script or by the
     # user manually (`ln -s ...`).
-    # Check if the existing symbolic link is valid or needs to be replaced.
-    if os.path.isdir(os.path.realpath("trace_orig")):
-        print("Symbolic link 'trace_orig' already exists (->'%s')." %
-              os.path.realpath("trace_orig"))
-        if trace_orig != os.path.realpath("trace_orig"):
-            print("It does not match the path given in options.yaml, so it "
-                  "will be replaced.")
-            os.remove("trace_orig")
-        else:
-            print("The existing link matches the path in 'options.yaml'. "
-                  "Nothing to do.")
-            sys.exit(0)
-        if trace_orig == os.path.abspath("trace_orig"):
-            print("You seem to have manually created the symbolic link for "
-                  "'trace_original'. I will leave it untouched.")
-            sys.exit(0)
-    else:
-        print("Existing symbolic link 'trace_orig' (->'%s') is invalid and "
-              "will be replaced" % trace_orig)
-        os.remove("trace_orig")
+    # We will just replace it.
+    os.remove("trace_orig")
 elif os.path.isdir("trace_orig"):
     print("'trace_orig' is an existing subdirectory.")
-    if trace_orig == os.path.abspath("trace_orig"):
-        print("We will leave it untouched because it matches the path given "
-              "in 'options.yaml'.")
-        sys.exit(0)
-    else:
-        print("I cannot create a symbolic link there. Please remove the "
-              "directory: '%s'" % os.path.abspath("trace_orig"))
-        sys.exit(1)
+    print("I cannot create a symbolic link there. Please remove the "
+          "directory: '%s'" % os.path.abspath("trace_orig"))
+    sys.exit(1)
 elif os.path.isfile("trace_orig"):
-    print("'trace_orig' exists, but is a file. Please remove it.")
+    print("'trace_orig' is an existing file.")
+    print("I cannot create a symbolic link there. Please remove the "
+          "file: '%s'" % os.path.abspath("trace_orig"))
     sys.exit(1)
 
 # Until here, the script has exited if we don’t need a new symbolic link.
