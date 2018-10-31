0cf5d64260784ffba5fe32404a78b90966a47d35
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2018-10-31 15:55:48 +0100

    Add accidentally removed YAML read and flake-format

--- scripts/aggregate_modern_trace.py
+++ scripts/aggregate_modern_trace.py
@@ -5,13 +5,15 @@ import sys
 import xarray as xr
 import yaml
 
+
 def drop_superfluous_trace_vars(data):
     """Drop all unneeded data variables from a TraCE-21ka xarray dataset."""
     return data.drop(["P0", "co2vmr", "gw", "hyai", "date", "date_written",
-                     "datesec", "hyai", "hyam", "hybi", "hybm", "mdt",
-                     "nbdate", "nbsec", "ndbase", "ndcur", "nlon", "nsbase",
-                     "nscur", "nsteph", "ntrk", "ntrm", "ntrn", "time_written",
-                     "wnummax"])
+                      "datesec", "hyai", "hyam", "hybi", "hybm", "mdt",
+                      "nbdate", "nbsec", "ndbase", "ndcur", "nlon", "nsbase",
+                      "nscur", "nsteph", "ntrk", "ntrm", "ntrn",
+                      "time_written", "wnummax"])
+
 
 def get_monthly_means(trace_file):
     """
@@ -26,7 +28,7 @@ def get_monthly_means(trace_file):
     data = xr.open_dataset(trace_file, decode_times=False)
     # Create list with numbers from 0 (January) to 12 (December).
     month_numbers = [i for i in range(12)]
-    data = drop_superfluous_trace_vars(data) # TODO: Do we need this?
+    data = drop_superfluous_trace_vars(data)  # TODO: Do we need this?
     # Repeat the months (assuming the dataset begins with January).
     year_count = len(data["time"]) // 12
     month_dim = month_numbers * year_count
@@ -36,6 +38,7 @@ def get_monthly_means(trace_file):
     # cell.
     data = data.groupby('time').mean('time')
     return data
+
 
 # Find relevant TraCE files of the modern time in a directory.
 # The file names are defined in the YAML options.
@@ -55,6 +58,7 @@ if not os.path.exists(heap):
     os.mkdir(heap)
 
 # Check if all original TraCE files are present.
+opts = yaml.load(open("options.yaml"))
 files = opts["modern_trace_files"]
 for f in files:
     if files[f] not in os.listdir("trace_orig"):
@@ -72,4 +76,3 @@ for f in files:
     out_file = os.path.join(heap, out_file)
     print("Writing file '%s'." % out_file)
     dataset.to_netcdf(out_file)
-
