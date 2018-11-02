9e2feb5de29bfddf05a8fd1700df416169ad995c
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2018-11-02 17:08:55 +0100

    Fix variable renaming add_PRECC_PRECL.sh

--- scripts/add_PRECC_PRECL.sh
+++ scripts/add_PRECC_PRECL.sh
@@ -8,14 +8,23 @@
 # Exit on failure of any command.
 set -e
 
-# Add PRECL and PRECC to PRECT.
 echo "Adding PRECC and PRECL to PRECT:"
-echo "'$1' + '$2' = '$3'"
-ncbo --overwrite --op_typ='add' "$1" "$2" "$3"
+echo -e "  '$1'\n+ '$2'\n= '$3'"
 
-echo "Renaming the variable to 'PRECT'."
-ncrename --variable PRECL,PRECT "$3"
+PRECC="$1"
+PRECL="$2"
+PRECT="$3"
 
-echo "Updating the 'long_name' attribute."
+# First copy PRECC file into output location so that we can rename its
+# variable, matching the variable of the other operand.
+cp "$PRECC" "$PRECT"
+ncrename --variable PRECC,PRECL "$PRECT"
+
+# Now we can add the matching variable name "PRECL".
+ncbo --overwrite --op_typ='add' -o "$PRECT" "$PRECL" "$PRECT"
+
+# Finally we need to name the sum appropriately "PRECT".
+ncrename --variable PRECL,PRECT "$PRECT"
+
 LONG_NAME="Total (convective and large-scale) precipitation rate (liq + ice)"
-ncatted --overwrite --attribute long_name,PRECT,m,c,"$LONG_NAME" "$3"
+ncatted --overwrite --attribute long_name,PRECT,m,c,"$LONG_NAME" "$PRECT"
