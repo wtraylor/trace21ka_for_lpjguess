c308d794c9a5ed01b36bd33e28bf30bbae09e26b
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-08 15:07:29 +0100

    Rewrite crop_file as function

--- trace_for_guess/crop_file.py
+++ trace_for_guess/crop_file.py
@@ -1,55 +1,32 @@
-#!/bin/python
-
 from termcolor import cprint
 import os
 import subprocess
-import sys
-import yaml
-
-# Name of this script file.
-scriptfile = os.path.basename(__file__) + ": "
-
-# Arguments:
-# 1) Input file
-# 2) Output file (will be overwritten!)
-
-if len(sys.argv) != 3:
-    cprint(scriptfile + "Please provide exactly two command line arguments.",
-           "red")
-    sys.exit(1)
-
-in_file = sys.argv[1]
-out_file = sys.argv[2]
-
-if not os.path.isfile(in_file):
-    cprint(scriptfile + "Input file does not exist.", "red")
-    sys.exit(1)
 
 
-region = yaml.load(open("options.yaml"))["region"]
-lon = region["lon"]
-lat = region["lat"]
+def crop_file(in_file, out_file, ext):
+    """Crop a NetCDF file to give rectangle.
 
-# TODO: Check if region is valid.
+    Args:
+        in_file: Input file path.
+        out_file: Output file path. It will be overwritten without warning.
+        ext: The rectangular region (extent) to crop to, given as a list of
+        [lon1, lon2, lat1, lat2].
 
-cprint(scriptfile + "'%s'..." % in_file, "green")
-status = subprocess.run(["ncks",
-                         "--overwrite",
-                         "--dimension", "lon,%.2f,%.2f" % (lon[0], lon[1]),
-                         "--dimension", "lat,%.2f,%.2f" % (lat[0], lat[1]),
-                         in_file,
-                         out_file]).returncode
-
-if status != 0:
-    cprint(scriptfile + "Cropping with `ncks` failed: Bad return code.",
-           "red")
-    cprint("Input file:", in_file, "red")
-    cprint("Output file:", out_file, "red")
-    sys.exit(1)
-
-if not os.path.isfile(out_file):
-    cprint(scriptfile + "Cropping with `ncks` failed: No output file "
-           "created.", "red")
-    cprint("Input file:", in_file, "red")
-    cprint("Output file:", out_file, "red")
-    sys.exit(1)
+    Raises:
+        RuntimeError
+    """
+    cprint("Cropping file '%s'..." % in_file, "green")
+    if not os.path.isfile(in_file):
+        raise FileNotFoundError("Input file doesn’t exist: '%s'" % in_file)
+    status = subprocess.run(["ncks",
+                             "--overwrite",
+                             "--dimension", "lon,%.2f,%.2f" % (ext[0], ext[1]),
+                             "--dimension", "lat,%.2f,%.2f" % (ext[0],
+                                                               ext[1]),
+                             in_file,
+                             out_file]).returncode
+    if status != 0:
+        raise RuntimeError("Cropping with `ncks` failed: Bad return code.")
+    if not os.path.isfile(out_file):
+        raise RuntimeError("Cropping with `ncks` failed: No output file "
+                           "created.")
