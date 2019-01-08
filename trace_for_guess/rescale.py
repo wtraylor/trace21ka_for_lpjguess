c302f05c804d58c0ac8441b7359568e43cb6ee60
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-08 15:53:55 +0100

    Rewrite rescale_file as function

--- trace_for_guess/rescale.py
+++ trace_for_guess/rescale.py
@@ -1,46 +1,40 @@
-#!/bin/python
-
 from termcolor import cprint
 import os
 import subprocess
-import sys
-import yaml
 
-# Name of this script file.
-scriptfile = os.path.basename(__file__) + ": "
 
-regrid_alg = yaml.load(open("options.yaml"))["regrid_algorithm"]
+def rescale_file(in_file, out_file, template_file, alg):
+    """Regrid a NetCDF file using NCO (i.e. the ncremap command).
 
-if len(sys.argv) != 3:
-    cprint(scriptfile + "Please provide exacty two command line arguments.", "red")
-    cprint(scriptfile + "<input file> <output file>", "red")
-    sys.exit(1)
-in_file = sys.argv[1]
-out_file = sys.argv[2]
+    Args:
+        in_file: Path of input file.
+        out_file: Output file path. It will be overwritten without warning.
+        template_file: Path to a NetCDF file that has the desired grid
+            resolution.
+        alg: ESMF regrid algorithm. See here:
+            http://www.earthsystemmodeling.org/esmf_releases/public/ESMF_6_3_0rp1/ESMF_refdoc/node3.html#SECTION03020000000000000000
 
-if not os.path.isfile(in_file):
-    cprint(scriptfile + "First argument is not a file: '%s'" % in_file, "red")
-    sys.exit(1)
+    Returns:
+        The output file (`out_file`).
 
-# Template grid.
-grid_templ = "heap/grid_template.nc"
-
-if not os.path.isfile(grid_templ):
-    cprint(scriptfile + "Grid template file does not exist: '%s'" % grid_templ,
-           "red")
-    sys.exit(1)
-
-cprint(scriptfile + "'%s' => '%s'" % (in_file, out_file), "green")
-
-status = subprocess.run(["ncremap",
-                         "--algorithm=%s" % regrid_alg,
-                         "--template_file=%s" % grid_templ,
-                         "--input_file=%s" % in_file,
-                         "--output_file=%s" % out_file])
-
-if not os.path.isfile(out_file):
-    cprint(scriptfile + "Regridding with `ncremap` failed: No output file "
-           "created.", "red")
-    cprint("Input file:", in_file, "red")
-    cprint("Output file:", out_file, "red")
-    sys.exit(1)
+    Raises:
+        FileNotFoundError: If `in_file` or `template_file` doesn’t exist.
+    """
+    cprint("Regridding '%s'..." % in_file, "green")
+    if not os.path.isfile(in_file):
+        raise FileNotFoundError("Input file doesn’t exist: '%s'" % in_file)
+    if not os.path.isfile(template_file):
+        raise FileNotFoundError("Template file doesn’t exist: '%s'" %
+                                template_file)
+    status = subprocess.run(["ncremap",
+                             "--algorithm=%s" % alg,
+                             "--template_file=%s" % template_file,
+                             "--input_file=%s" % in_file,
+                             "--output_file=%s" % out_file])
+    if status != 0:
+        raise RuntimeError("Regridding with `ncremap` failed: Bad return "
+                           "code.")
+    if not os.path.isfile(out_file):
+        raise RuntimeError("Regridding with `ncremap` failed: No output file "
+                           "created.")
+    return out_file
