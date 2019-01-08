20c66ebb0aa2a356f3b6506f53456018cdd07dea
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-08 14:21:48 +0100

    Add message to unzip()

--- trace_for_guess/unzip.py
+++ trace_for_guess/unzip.py
@@ -1,7 +1,9 @@
+from termcolor import cprint
 from zipfile import ZipFile
 
 
 def unzip(filename, targetdir):
     """Decompress a zip file into a target directory."""
+    cprint("Unzipping '%s' into '%s'..." % (filename, targetdir))
     with ZipFile(filename, "r") as zip_ref:
         zip_ref.extractall(targetdir)
