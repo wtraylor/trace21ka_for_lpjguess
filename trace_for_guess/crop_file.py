eb190011254b91d218df1259d41c9b6f569eca24
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-08 15:27:09 +0100

    Add crop_file_list() function

--- trace_for_guess/crop_file.py
+++ trace_for_guess/crop_file.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def crop_file(in_file, out_file, ext):
-    """Crop a NetCDF file to give rectangle.
+    """Crop a NetCDF file to give rectangle using NCO.
 
     Args:
         in_file: Input file path.
@@ -12,8 +12,12 @@ def crop_file(in_file, out_file, ext):
         ext: The rectangular region (extent) to crop to, given as a list of
         [lon1, lon2, lat1, lat2].
 
+    Returns:
+        Name of output file (same as `out_file`).
+
     Raises:
-        RuntimeError
+        FileNotFoundError: `in_file` doesn’t exist.
+        RuntimeError: NCKS failed or no output file was created.
     """
     cprint("Cropping file '%s'..." % in_file, "green")
     if not os.path.isfile(in_file):
@@ -30,3 +34,20 @@ def crop_file(in_file, out_file, ext):
     if not os.path.isfile(out_file):
         raise RuntimeError("Cropping with `ncks` failed: No output file "
                            "created.")
+    return out_file
+
+
+def crop_file_list(filelist, out_dir, ext):
+    """Crop all files in the list and store the output files in a directory.
+
+    Args:
+        filelist: List of input NetCDF file paths.
+        out_dir: Path to output directory.
+        ext: The rectangular region (extent) to crop to, given as a list of
+        [lon1, lon2, lat1, lat2].
+
+    Returns:
+        List of paths to cropped files.
+    """
+    return [crop_file(f, os.path.join(out_dir, os.path.basename(f)), ext)
+            for f in filelist]
