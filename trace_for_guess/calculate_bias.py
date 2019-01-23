195917766a2779c1518b67ad436e2be41416b2e3
Wolfgang Traylor <living@posteo.de>
2019-01-23 15:48:55 +0000

    Include all operations in try block

--- trace_for_guess/calculate_bias.py
+++ trace_for_guess/calculate_bias.py
@@ -38,25 +38,25 @@ def calculate_bias(trace_file, trace_var, cru_file, cru_var, bias_file):
     cprint(f"'{trace_file}' x '{cru_file}' -> '{bias_file}'", 'yellow')
     # Open and load the files completely. They need to be in the RAM for
     # calculation.
-    trace = xr.open_dataset(trace_file, decode_times=False).load()
-    cru = xr.open_dataset(cru_file, decode_times=False).load()
-    # The values of the 'time' dimensions of the CRU and the TraCE dataset
-    # must match in order to perform calculation. So we just overwrite the
-    # values with 0 to 11 as the month numbers, assuming that the CRU
-    # record also starts with January.
-    cru["time"].values = [i for i in range(12)]
-    # Rename the variable in the CRU file to the TraCE variable name.
-    cru = cru.rename({cru_var: trace_var})
-    if trace_var == "TREFHT":
-        bias = trace[trace_var] - cru[trace_var]
-    elif trace_var == "PRECT":
-        # TODO Rename precipitation variable
-        bias = trace[trace_var] / cru[trace_var]
-    else:
-        raise NotImplementedError("Arithmetic operation not defined for"
-                                  "variable '%s'." % trace_var)
     try:
-        bias.to_netcdf(bias_file, mode='w')
+        trace = xr.open_dataset(trace_file, decode_times=False).load()
+        cru = xr.open_dataset(cru_file, decode_times=False).load()
+        # The values of the 'time' dimensions of the CRU and the TraCE dataset
+        # must match in order to perform calculation. So we just overwrite the
+        # values with 0 to 11 as the month numbers, assuming that the CRU
+        # record also starts with January.
+        cru["time"].values = [i for i in range(12)]
+        # Rename the variable in the CRU file to the TraCE variable name.
+        cru = cru.rename({cru_var: trace_var})
+        if trace_var == "TREFHT":
+            bias = trace[trace_var] - cru[trace_var]
+        elif trace_var == "PRECT":
+            # TODO Rename precipitation variable
+            bias = trace[trace_var] / cru[trace_var]
+        else:
+            raise NotImplementedError("Arithmetic operation not defined for"
+                                    "variable '%s'." % trace_var)
+            bias.to_netcdf(bias_file, mode='w')
     except:
         if os.path.isfile(bias_file):
             cprint(f"Removing file '{bias_file}'.", 'red')
