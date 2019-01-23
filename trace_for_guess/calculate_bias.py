4f43095c0250c49e5e2e6753c4d7e28d3ea73a32
Wolfgang Traylor <living@posteo.de>
2019-01-23 15:45:53 +0000

    Skip if file exists

--- trace_for_guess/calculate_bias.py
+++ trace_for_guess/calculate_bias.py
@@ -1,4 +1,4 @@
-from os.path import isfile
+import os
 
 from termcolor import cprint
 
@@ -26,11 +26,14 @@ def calculate_bias(trace_file, trace_var, cru_file, cru_var, bias_file):
         NotImplementedError: The variable in the TraCE file is not
             implemented.
     """
-    if not isfile(trace_file):
+    if not os.path.isfile(trace_file):
         raise FileNotFoundError(
             "TraCE-21ka mean file doesn’t exist: '%s'" % trace_file)
-    if not isfile(cru_file):
+    if not os.path.isfile(cru_file):
         raise FileNotFoundError("CRU mean file doesn’t exist: '%s'" % cru_file)
+    if os.path.isfile(bias_file):
+        cprint(f"Skipping: '{bias_file}'", 'cyan')
+        return bias_file
     cprint('Calculating bias:', 'yellow')
     cprint(f"'{trace_file}' x '{cru_file}' -> '{bias_file}'", 'yellow')
     # Open and load the files completely. They need to be in the RAM for
