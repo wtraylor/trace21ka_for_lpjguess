99b63e7ee88d7718482e082c2c5406f60b7770c4
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-24 15:27:15 +0100

    Use NCO commands instead of Xarray to convert time

--- trace_for_guess/convert_time_unit.py
+++ trace_for_guess/convert_time_unit.py
@@ -1,50 +1,40 @@
 import os
+import shutil
+import subprocess
 
-import xarray as xr
 import yaml
 from termcolor import cprint
 
 
-def convert_time_unit(trace_file, out_file):
-    """Convert kaBP time unit to TODO...
+def convert_time_unit(trace_file):
+    """Convert kaBP time unit to calendar using NCO commands.
 
     Args:
         trace_file: File path to TraCE-21ka file with kaBP unit.
-        out_file: Output file with converted time unit.
 
     Raises:
         FileNotFoundError: If `trace_file` does not exist.
-
-    Returns:
-        Path to output file, same as parameter `out_file`.
     """
     if not os.path.isfile(trace_file):
         raise FileNotFoundError(f"Could not find TraCE file '{trace_file}'.")
-    if os.path.isfile(out_file):
-        cprint(f"Skipping: '{out_file}'", 'cyan')
-        return out_file
-    out_dir = os.path.dirname(out_file)
-    if not os.path.isdir(out_dir):
-        cprint(f"Directory '{out_dir}' does not exist yet. I will create it.",
-            'yellow')
-        os.makedirs(out_dir)
-    cprint(f"Converting time unit of TraCE file '{trace_file}'.", 'yellow')
     attrs = yaml.load(open('options.yaml'))['nc_attributes']['time']
+    cprint(f"Converting kaBP time unit in TraCE file '{trace_file}'.",
+           'yellow')
+    if shutil.which('ncatted') is None:
+        raise RuntimeError("Executable `ncatted` not found.")
+    if shutil.which('ncap2') is None:
+        raise RuntimeError("Executable `ncap2` not found.")
     try:
-        with xr.open_dataset(trace_file) as dataset:
-            kaBP = - dataset.time.values
-            years_BP = kaBP * 1000
-            years_BC = years_BP - 1950
-            years_since_20050_BC = 20050 - years_BC
-            days_since_20050_BC = 365 * years_since_20050_BC
-            dataset.time.values = days_since_20050_BC
-            dataset.time.attrs['units'] = attrs['units']
-            dataset.time.attrs['calendar'] = attrs['calendar']
-            dataset.to_netcdf(out_file, mode='w')
+        time_script = 'time=trunc((time+22.0)*1000*365)'
+        # --append flag overwrites existing time dimension.
+        subprocess.run(['ncap2', '--append', '--script', time_script,
+                        trace_file], check=True)
+        units = attrs['units']
+        calendar = attrs['calendar']
+        subprocess.run(['ncatted', '--overwrite',
+                        '--attribute', f'units,time,o,c,{units}',
+                        '--attribute', f'calendar,time,o,c,{calendar}',
+                        trace_file])
     except:
-        if os.path.isfile(out_file):
-            cprint(f"Removing file '{trace_file}'.", 'red')
-            os.remove(out_file)
-    assert(os.path.isfile(out_file))
-    cprint(f"Created TraCE file with converted time unit in
-    return out_file
+        cprint(f"Removing file '{trace_file}'.", 'red')
+        os.remove(trace_file)
