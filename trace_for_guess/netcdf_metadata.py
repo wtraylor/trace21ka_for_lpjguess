6e1478775ad4657772b6941190cf74e94f57412e
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-25 12:32:47 +0100

    Start preparing set_metadata()

--- trace_for_guess/netcdf_metadata.py
+++ trace_for_guess/netcdf_metadata.py
@@ -1,5 +1,8 @@
 import os
+import xarray as xr
+import subprocess
 
+import shutil
 import yaml
 
 
@@ -15,15 +18,28 @@ def set_attributes(da, var):
         da.attrs[key] = val
 
 
-def set_metadata(trace_file, var):
+def set_metadata(trace_file, trace_var):
     """Set NetCDF metadata of given file to CF standards for LPJ-GUESS.
 
     Args:
         trace_file: Full path to TraCE-21ka NetCDF file.
-        var: The TraCE variable in the file.
+        trace_var: The TraCE variable in the file.
 
     Raises:
         FileNotFoundError: If `trace_file` does not exist.
+        RuntimeError: Executable `ncatted` is not in the PATH
     """
     if not os.path.isfile(trace_file):
         raise FileNotFoundError(f"TraCE file doesn’t exist: '{trace_file}'")
+    if shutil.which('ncatted') is None:
+        raise RuntimeError("Executable `ncatted` not found.")
+    attributes = yaml.load(open("options.yaml"))["nc_attributes"][trace_var]
+    ncatted_args = list()
+    for (var, a) in attributes:
+        with xr.open_dataset(f, decode_times=False) as ds:
+            if var not in ds:
+                continue
+        for (key, val) in attrs:
+            ncatted_args += ['--attribute', f'{key},{var},o,c,{val}']
+    subprocess.run(['ncatted', '--overwrite'] + ncatted_args
+                    + [trace_file], check=True)
