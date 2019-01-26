f825368dd1ead4f53f85f95ed062668fe12d2a74
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-01-26 12:48:34 +0100

    Add (unfinished) function create_co2_file()

--- trace_for_guess/co2.py
+++ trace_for_guess/co2.py
@@ -6,28 +6,32 @@ from termcolor import cprint
 from trace_for_guess.skip import skip
 
 
-def create_co2_file(trace_file, co2_file):
-    """Create a CO₂ file for LPJ-GUESS.
+def create_co2_file(trace_files, co2_file):
+    """Create a CO₂ file for LPJ-GUESS from TraCE-21ka files.
+
+    TODO: The `year` column must be continuously incrementing calendar years.
 
     Args:
-        trace_file: Path to TraCE file.
+        trace_files: A list with contiguous TraCE-21ka files.
         co2_file: Output file.
 
     Raises:
-        FileNotFoundError: `trace_file` does not exist.
+        FileNotFoundError: A file in `trace_files` does not exist.
     """
-    if not os.path.isfile(trace_file):
-        raise FileNotFoundError(f"Input file not found: '{trace_file}'")
-    if skip(trace_file, co2_file):
+    for f in trace_files:
+        if not os.path.isfile(f):
+            raise FileNotFoundError(f"Input file not found: '{f}'")
+    if skip(trace_files, co2_file):
         return
     try:
-        with xr.open_dataset(trace_file, decode_times=False) as ds, \
-                open(co2_file, 'w') as f:
-            co2 = ds['co2vmr']
-            for i in range(len(co2.values)):
-                year = co2['time'].values[i]
-                value = co2.values[i]
-                f.write(f'{year}\t{value}\n')
+        with open(co2_file, 'w') as out_file:
+            for f in trace_files:
+                with xr.open_dataset(trace_file, decode_times=False) as ds:
+                    co2 = ds['co2vmr']
+                    for i in range(len(co2.values)):
+                        year = co2['time'].values[i]
+                        value = co2.values[i]
+                        out_file.write(f'{year}\t{value}\n')
     except Exception:
         if os.path.isfile(co2_file):
             cprint(f"Removing file '{co2_file}'.", 'red')
