5e311cd313d75d49d5756544f003285b88af694b
Wolfgang Traylor <living@posteo.de>
2019-01-30 15:09:28 +0000

    Tidy up clean.py
    - delete 0_input separately
    - delete subdirectories in heap

--- trace_for_guess/clean.py
+++ trace_for_guess/clean.py
@@ -4,6 +4,7 @@
 
 import glob
 import os
+import sys
 
 import yaml
 from termcolor import cprint
@@ -13,31 +14,66 @@ def confirm(prompt, filelist):
     """Prompt the user for confirmation."""
     while True:
         cprint(prompt, 'green')
-        cprint('(y)es, (n)o, or (l)ist files.', 'green')
+        cprint('(y)es, (n)o, (l)ist files, or (q)uit.', 'green')
         answer = input('')
         if answer in 'yY':
             return True
         elif answer in 'nN':
             return False
         elif answer in 'lL':
-            print(filelist)
+            for f in filelist:
+                print(f)
+        elif answer in 'qQ':
+            cprint('User canceled cleaning.', 'red')
+            sys.exit(0)
         else:
-            cprint('Please enter y, n, or l.', 'green')
+            cprint('Please enter y, n, l, or q.', 'green')
 
 
 dirs = yaml.load(open('options.yaml'))['directories']
+heap = dirs['heap']
+heap_input = os.path.join(heap, '0_input')
+out_dir = dirs['output']
 
-heap_files = glob.glob(os.path.join(dirs['heap'], '**'))
-if heap_files:
-    if confirm('Do you want to delete all intermediary files in the heap?',
-               heap_files):
-        for f in heap_files:
+extracted_files = glob.glob(os.path.join(heap_input, '**'))
+if extracted_files:
+    if confirm('Do you want to delete all extracted input files in the heap?',
+               extracted_files):
+        for f in extracted_files:
             cprint(f"Deleting '{f}''", 'yellow')
             os.remove(f)
 else:
-    cprint('No files to delete in the heap directory.', 'green')
+    cprint('No extracted input files to delete in the heap directory.',
+           'green')
 
-out_files = glob.glob(os.path.join(dirs['output'], '**'))
+# Use absolute paths for extracted files in order to compare them with other
+# heap files.
+dont_delete = list()
+for f in extracted_files:
+    dont_delete += [os.path.abspath(f)]
+
+heap_files = list()
+for root, dirs, files in os.walk(heap):
+    for f in files:
+        f = os.path.join(root, f)
+        if not os.path.abspath(f) in dont_delete:
+            heap_files += [f]
+if heap_files:
+    if confirm('Do you want to delete all intermediary files in the heap?',
+               heap_files):
+        for root, dirs, files in os.walk(heap, topdown=False):
+            if os.path.join(root, f) in dont_delete:
+                continue
+            for f in files:
+                cprint(f"Deleting '{f}''", 'yellow')
+                os.remove(os.path.join(root, f))
+            for d in dirs:
+                cprint(f"Deleting directory '{d}''", 'yellow')
+                os.rmdir(os.path.join(root, d))
+else:
+    cprint('No processed files to delete in the heap directory.', 'green')
+
+out_files = glob.glob(os.path.join(out_dir, '**'))
 if out_files:
     if confirm("Do you want to delete all output files?", out_files):
         for f in out_files:
@@ -47,3 +83,4 @@ else:
     cprint('No output files to delete.', 'green')
 
 cprint('Done', 'green')
+sys.exit(0)
