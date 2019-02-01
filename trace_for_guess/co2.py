cc67d82e786b26c7a787975f109ae51074c34807
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-02-01 16:40:48 +0100

    Simplify CO₂ file of concatenated NetCDF file
    Simply pass the concatenated file to create_co2_files()

--- trace_for_guess/co2.py
+++ trace_for_guess/co2.py
@@ -1,13 +1,10 @@
-import re
 import os
 
 import subprocess
-import shutil
 from termcolor import cprint
 
 from trace_for_guess.filenames import get_co2_filename
-from trace_for_guess.netcdf_metadata import (get_metadata_from_trace_file,
-                                             get_metadata_from_trace_files)
+from trace_for_guess.netcdf_metadata import get_metadata_from_trace_file
 from trace_for_guess.skip import skip
 
 
@@ -23,7 +20,7 @@ def get_co2_values(trace_file):
     if not os.path.isfile(trace_file):
         raise FileNotFoundError(f"Input file not found: '{trace_file}'")
     stdout = subprocess.run(['cdo', 'output', '-yearmean',
-                                '-selvar,co2vmr', trace_file], check=True,
+                             '-selvar,co2vmr', trace_file], check=True,
                             encoding='utf-8', capture_output=True).stdout
     co2_vals = [float(v) for v in stdout.split()]
     del stdout
@@ -35,19 +32,14 @@ def get_co2_values(trace_file):
     return dict(zip(years, co2_vals))
 
 
-def create_co2_files(trace_files, out_dir, concat):
+def create_co2_files(trace_files, out_dir):
     """Create CO₂ files for LPJ-GUESS from TraCE-21ka files.
 
-    For each single input file, there will be a CO₂ file created. In addition,
-    a concatenated CO₂ can be created covering all TraCE files in the list.
-
-    TODO: The `year` column must be continuously incrementing calendar years.
+    For each single input file, there will be a CO₂ file created.
 
     Args:
         trace_files: A list with contiguous TraCE-21ka files.
         out_dir: Output directory.
-        concat: Whether to create an additional CO₂ file covering all TraCE
-            files.
 
     Raises:
         FileNotFoundError: A file in `trace_files` does not exist.
@@ -57,38 +49,27 @@ def create_co2_files(trace_files, out_dir, concat):
         if not os.path.isfile(f):
             raise FileNotFoundError(f"Input file not found: '{f}'")
         metadata = get_metadata_from_trace_file(f)
-        co2_files[f] = (
-            os.path.join(out_dir, get_co2_filename(metadata['first_year'],
-                                                   metadata['last_year']))
-        )
-        del metadata
-    metadata = get_metadata_from_trace_files(trace_files)
-    co2_concat = os.path.join(out_dir, get_co2_filename(metadata['first_year'],
-                                                        metadata['last_year']))
-    if skip(trace_files, list(co2_files.values()) + [co2_concat]):
+        basename = get_co2_filename(metadata['first_year'],
+                                    metadata['last_year'])
+        co2_files[f] = os.path.join(out_dir, basename)
+    if skip(trace_files, list(co2_files.values())):
         return
     try:
-        with open(co2_concat, 'w') as out_concat:
-            for f in trace_files:
-                co2_vals = get_co2_values(f)
-                with open(co2_files[f], 'w') as out_single:
-                    for year in co2_vals:
-                        val = co2_vals[year]
-                        line = f'{year}\t{val}\n'
-                        out_concat.write(line)
-                        out_single.write(line)
+        for f in trace_files:
+            co2_vals = get_co2_values(f)
+            with open(co2_files[f], 'w') as out:
+                for year in co2_vals:
+                    val = co2_vals[year]
+                    line = f'{year}\t{val}\n'
+                    out.write(line)
     except Exception:
-        for f in list(co2_files.values()) + [co2_concat]:
+        for f in list(co2_files.values()):
             if os.path.isfile(f):
                 cprint(f"Removing file '{f}'.", 'red')
                 os.remove(f)
         raise
-    assert os.path.isfile(co2_concat)
     for f in co2_files.values():
         assert os.path.isfile(f)
-    if not concat:
-        os.remove(co2_concat)
-    cprint('Successfully created files:', 'green')
-    cprint('\t' + co2_concat, 'green')
+    cprint('Successfully created CO₃ file:', 'green')
     for f in co2_files.values():
         cprint('\t' + f, 'green')
