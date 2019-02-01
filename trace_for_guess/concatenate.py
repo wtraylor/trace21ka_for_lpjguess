2901ead442745cf2e237dc2e6cf8d05c4beca602
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-02-01 11:07:42 +0100

    Use `cdo cat` instead of `nccat`
    NCO was complaining about missing record dimensions in PRECT. I don’t
    know why.

--- trace_for_guess/concatenate.py
+++ trace_for_guess/concatenate.py
@@ -9,7 +9,7 @@ from trace_for_guess.skip import skip
 
 
 def cat_files(filelist, out_file):
-    """Concatenate a list of NetCDF files using NCO (i.e. ncrcat).
+    """Concatenate a list of NetCDF files using CDO.
 
     Args:
         filelist: List of input file paths.
@@ -20,25 +20,24 @@ def cat_files(filelist, out_file):
 
     Raises:
         FileNotFoundError: A file in `filenames` wasn’t found.
-        RuntimeError: The command `ncrcat` is not in the PATH.
+        RuntimeError: The command `cdo` is not in the PATH.
     """
     for f in filelist:
         if not os.path.isfile(f):
             raise FileNotFoundError("Input file not found: '%s'" % f)
     if skip(filelist, out_file):
         return out_file
-    if shutil.which("ncrcat") is None:
-        raise RuntimeError('The command `ncrcat` could not be found.')
+    if shutil.which('cdo') is None:
+        raise RuntimeError('The command `cdo` could not be found.')
     cprint('Concatenating files:', 'yellow')
     for f in filelist:
         cprint('\t' + f, 'yellow')
     try:
-        subprocess.run(['ncrcat'] + filelist + [out_file], check=True)
+        subprocess.run(['cdo', 'cat'] + filelist + [out_file], check=True)
     except Exception:
-        for f in [out_file] + glob.glob(out_file + 'pid*.ncrcat.tmp'):
-            if os.path.isfile(out_file):
-                cprint(f"Removing file '{f}'.", 'red')
-                os.remove(out_file)
+        if os.path.isfile(out_file):
+            cprint(f"Removing file '{out_file}'.", 'red')
+            os.remove(out_file)
         raise
     assert os.path.isfile(out_file)
     cprint(f"Created file '{out_file}'.", 'green')
