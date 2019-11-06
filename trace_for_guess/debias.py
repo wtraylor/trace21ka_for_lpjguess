507086b54b7112056c6a5e66ecb7f91f233df525
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-11-06 12:19:02 +0100

    Correct debias_trace_file() with syntax checker

--- trace_for_guess/debias.py
+++ trace_for_guess/debias.py
@@ -53,7 +53,7 @@ def debias_trace_file(trace_file, bias_file, out_file):
             # How many years are in the monthly TraCE file?
             years = len(trace[var]) // 12
             # Repeat the monthly bias values for each year.
-            bias = xr.concat([bias]*years, dim='time')
+            bias = xr.concat([bias] * years, dim='time')
             # Overwrite the time dimension. If it does not match the TraCE
             # file, the arithmetic operation does not work.
             bias.time.values = trace.time.values
@@ -63,7 +63,7 @@ def debias_trace_file(trace_file, bias_file, out_file):
             elif var == "PRECT":
                 output = trace[var] / bias
             elif var == 'CLDTOT':
-                output = trace[var] ** bias
+                output = trace[var]**bias
             else:
                 raise NotImplementedError("No bias correction defined for "
                                           "variable '%s'." % var)
@@ -78,19 +78,18 @@ def debias_trace_file(trace_file, bias_file, out_file):
     return out_file
 
 
-def debias_fsds_file(fsds_file, fsdsc_file, fsdscl_file,
-                      cldtot_debiased_file):
+def debias_fsds_file(fsdsc_file, fsdscl_file, cldtot_file, out_file):
     """Apply bias-correction to an FSDS TraCE-21ka file.
 
     Args:
-        fsds_file: Name of the original (biased) TraCE FSDS file.
-        cldtot_debiased_file: The bias-corrected TraCE CLDTOT file (total cloud
+        cldtot_file: The bias-corrected TraCE CLDTOT file (total cloud
             cover).
         fsdsc_file: Name of the original (biased) TraCE FSDSC file (solar
             radiation in clear sky)
         fsdscl_file: Name of the original (biased) TraCE FSDSCL file (solar
             radiation under fully clouded sky).
-        out_file: Bias-corrected output file name (will not be overwritten).
+        out_file: Bias-corrected output file name for FSDS variable (will not
+            be overwritten).
 
     Returns:
         The name of the new file (equal to `out_file`).
@@ -99,31 +98,28 @@ def debias_fsds_file(fsds_file, fsdsc_file, fsdscl_file,
         FileNotFoundError: One of the input files doesn’t exist.
         RuntimeError: If no output file was produced.
     """
-    if not os.path.isfile(fsds_file):
-        raise FileNotFoundError("FSDS file does not exist: '%s'" % fsds_file)
     if not os.path.isfile(fsdsc_file):
         raise FileNotFoundError("FSDSC file does not exist: '%s'" % fsdsc_file)
     if not os.path.isfile(fsdscl_file):
         raise FileNotFoundError("FSDSCL file does not exist: '%s'" %
                                 fsdscl_file)
-    if not os.path.isfile(cldtot_debiased_file):
+    if not os.path.isfile(cldtot_file):
         raise FileNotFoundError("CLDTOT file does not exist: '%s'" %
-                                cldtot_debiased_file)
-    if skip([fsds_file, fsdsc_file, fsdscl_file, cldtot_debiased_file],
-            out_file):
+                                cldtot_file)
+    if skip([fsdsc_file, fsdscl_file, cldtot_file], out_file):
         return out_file
     out_dir = os.path.dirname(out_file)
     if not os.path.isdir(out_dir):
         cprint(f"Directory '{out_dir}' does not exist yet. I will create it.",
                'yellow')
         os.makedirs(out_dir)
-    cprint(f"Debiasing FSDS file '{fsds_file}'...", 'yellow')
+    cprint(f"Creating debiased FSDS file to '{out_file}'...", 'yellow')
     try:
-        with xr.open_dataset(fsds_file, decode_times=False) as fsds,
-        xr.open_dataset(fsdsc_file, decode_times=False) as fsdsc,
-        xr.open_dataset(fsdscl_file, decode_times=False) as fsdscl,
-        xr.open_dataset(cldtot_debiased_file, decode_times=False) as cldtot:
+        with xr.open_dataset(fsdsc_file, decode_times=False) as fsdsc,\
+                xr.open_dataset(fsdscl_file, decode_times=False) as fsdscl,\
+                xr.open_dataset(cldtot_file, decode_times=False) as cldtot:
             output = (1 - cldtot) * fsdsc + cldtot * fsdscl
+            output.to_netcdf(out_file, mode='w', engine='netcdf4')
     except Exception:
         if os.path.isfile(out_file):
             cprint(f"Removing file '{out_file}'.", 'red')
